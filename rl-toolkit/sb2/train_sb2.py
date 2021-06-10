616f119fe395cd3df26c271cc5df7e4645073a2a
mlap <marcuslapeyrolerie@me.com>
2021-06-10 00:57:18 +0000

    Trying to pass checks

--- rl-toolkit/sb2/train_sb2.py
+++ rl-toolkit/sb2/train_sb2.py
@@ -1,22 +1,28 @@
-import optuna
-from optuna.visualization import *
-
-from simulate_vec_sb2 import simulate_mdp_vec
-from train_utils import parse_hyperparams
-from stable_baselines import A2C, ACKTR, PPO2
-from stable_baselines.common import make_vec_env
-from stable_baselines.common.policies import LstmPolicy
-
+import argparse
 import json
+import os
+
 import gym
 import gym_conservation
-import argparse
 import tensorflow as tf
+from stable_baselines import A2C, ACKTR, PPO2
+from train_utils import parse_hyperparams
 
-# Argument parsing block; to get help on this from CL run `python tune_sb3.py -h`
+os.environ["TF_CPP_MIN_LOG_LEVEL"] = "3"
+
+# To avoid GPU memory hogging by TF
+config = tf.ConfigProto()
+config.gpu_options.allow_growth = True
+sess = tf.Session(config=config)
+
+# Argument parsing block; for help run `python tune_sb3.py -h`
 parser = argparse.ArgumentParser()
-parser.add_argument("-e", "--env", type=str, help="Environment name", default="conservation-v0")
-parser.add_argument("--study-name", type=str, help="Study name", default="test")
+parser.add_argument(
+    "-e", "--env", type=str, help="Environment name", default="conservation-v0"
+)
+parser.add_argument(
+    "--study-name", type=str, help="Study name", default="test"
+)
 parser.add_argument(
     "-a",
     "--algorithm",
@@ -53,15 +59,18 @@ algo_utils = {
     "ppo2": (PPO2,),
 }
 
-def main():
-  params, CustomLSTMPolicy, env = parse_hyperparams(args)
-  # Instatiating model and performing training
-  model = algo_utils[args.algorithm][0](CustomLSTMPolicy, env, verbose=2, **params)
-  model.learn(total_timesteps=int(args.n_timesteps))
-  # Evaluating the agent and reporting the mean cumulative reward
-  eval_env = gym.make(args.env)
-  eval_df = simulate_mdp_vec(env, eval_env, model, args.n_eval_episodes)
 
-if __name__ =="__main__":
-  main()
-  
+def main():
+    params, CustomLSTMPolicy, env = parse_hyperparams(args)
+    # Instatiating model and performing training
+    model = algo_utils[args.algorithm][0](
+        CustomLSTMPolicy, env, verbose=2, **params
+    )
+    model.learn(total_timesteps=int(args.n_timesteps))
+    # Evaluating the agent and reporting the mean cumulative reward
+    # eval_env = gym.make(args.env)
+    # eval_df = simulate_mdp_vec(env, eval_env, model, args.n_eval_episodes)
+
+
+if __name__ == "__main__":
+    main()
--- rl-toolkit/sb2/train_utils.py
+++ rl-toolkit/sb2/train_utils.py
@@ -1,66 +1,64 @@
-import optuna
 import gym
+import optuna
 from stable_baselines.common import make_vec_env
 from stable_baselines.common.policies import LstmPolicy
 
 
 def parse_hyperparams(args):
-  storage_name = f"sqlite:///tuning_studies/{args.study_name}.db"
-  study = optuna.load_study(study_name=args.study_name, storage=storage_name)
-  trial = study.best_trial
-  params = trial.params
-  vec = False
-  # Make the environment depending on number of environments in params
-  try:
-    env = make_vec_env(
-        lambda: gym.make(args.env, **args.env_kwargs),
-        n_envs=params["n_envs"],
-    )
-    params.pop('n_envs')
-    vec = True
-  except:
-    env = gym.make(args.env)
-  # Constructing the network architecture
-      # Mapping net_arch to actual network architectures for SB
-  net_arch = {
-      "small": dict(pi=[64, 64], vf=[64, 64]),
-      "med": dict(pi=[256, 256], vf=[256, 256]),
-      "large": dict(pi=[400, 400], vf=[400, 400]),
-  }[params["net_arch"]]
-  # Creating a custom LSTM policy
+    storage_name = f"sqlite:///tuning_studies/{args.study_name}.db"
+    study = optuna.load_study(study_name=args.study_name, storage=storage_name)
+    trial = study.best_trial
+    params = trial.params
+    # Make the environment depending on number of environments in params
+    try:
+        env = make_vec_env(
+            lambda: gym.make(args.env, **args.env_kwargs),
+            n_envs=params["n_envs"],
+        )
+        params.pop("n_envs")
+    except:
+        env = gym.make(args.env)
+    # Constructing the network architecture
+    # Mapping net_arch to actual network architectures for SB
+    net_arch = {
+        "small": dict(pi=[64, 64], vf=[64, 64]),
+        "med": dict(pi=[256, 256], vf=[256, 256]),
+        "large": dict(pi=[400, 400], vf=[400, 400]),
+    }[params["net_arch"]]
+    # Creating a custom LSTM policy
 
-  class CustomLSTMPolicy(LstmPolicy):
-      def __init__(
-          self,
-          sess,
-          ob_space,
-          ac_space,
-          n_env,
-          n_steps,
-          n_batch,
-          n_lstm=params["n_lstm"],
-          reuse=False,
-          **_kwargs
-      ):
-          super().__init__(
-              sess,
-              ob_space,
-              ac_space,
-              n_env,
-              n_steps,
-              n_batch,
-              n_lstm,
-              reuse,
-              net_arch=[100, "lstm", net_arch],
-              layer_norm=True,
-              feature_extraction="mlp",
-              **_kwargs
-          )
-          
-  # Deleting keys that can't be used in SB models
-  keys_to_delete = ["batch_size", "n_lstm", "net_arch", "joker"]
-  if "lambda" in params:
-    keys_to_delete.append("lambda")
-    params['lam'] = params['lambda']
-  [params.pop(key) for key in keys_to_delete if key in params]
-  return params, CustomLSTMPolicy, env
+    class CustomLSTMPolicy(LstmPolicy):
+        def __init__(
+            self,
+            sess,
+            ob_space,
+            ac_space,
+            n_env,
+            n_steps,
+            n_batch,
+            n_lstm=params["n_lstm"],
+            reuse=False,
+            **_kwargs,
+        ):
+            super().__init__(
+                sess,
+                ob_space,
+                ac_space,
+                n_env,
+                n_steps,
+                n_batch,
+                n_lstm,
+                reuse,
+                net_arch=[100, "lstm", net_arch],
+                layer_norm=True,
+                feature_extraction="mlp",
+                **_kwargs,
+            )
+
+    # Deleting keys that can't be used in SB models
+    keys_to_delete = ["batch_size", "n_lstm", "net_arch", "joker"]
+    if "lambda" in params:
+        keys_to_delete.append("lambda")
+        params["lam"] = params["lambda"]
+    [params.pop(key) for key in keys_to_delete if key in params]
+    return params, CustomLSTMPolicy, env
